3a503d85c3c4f930687be4f32a7714ea1dbd37e2
0Pablo00 <pafer0008@gmail.com>
2026-03-10 12:20:41 -0300

    Prueba de Live Updates - cambio visible en HTML

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'tareas-mfi',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: 'b05ebefd',
+      channel: 'production',
+      autoUpdateMethod: 'auto',
+      enabled: true
+    }
+  }
 };
 
 export default config;
